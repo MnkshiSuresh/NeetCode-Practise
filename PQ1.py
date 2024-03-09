043d4e0872e7ee8b8d7c727c4eaf66259247c65c
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-03-09 23:59:37 +0530

    Update PQ1.py

--- PQ1.py
+++ PQ1.py
@@ -3,7 +3,7 @@
 #Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
 
 #Note that multiple kids can have the greatest number of candies.
-
+ffff
 
 ffffffff
 fffffffff
